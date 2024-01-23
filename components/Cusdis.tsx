58db3bcc7e6da3d52d47d103d6bc9be508e5528e
Yanke <37400776+Yanke-Bioinfor@users.noreply.github.com>
2024-01-23 16:31:18 +0800

    Update Cusdis.tsx

--- components/Cusdis.tsx
+++ components/Cusdis.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.css'
 
 export const CusdisImpl: React.FC = () => {
   return (
+    <>
           <div id="cusdis_thread"
         data-host="https://cusdis.com"
         data-app-id="a260ec53-7bfc-46c1-a7ac-6e74bbd526d8"
@@ -12,6 +13,7 @@ export const CusdisImpl: React.FC = () => {
         data-page-title={title}
         ></div>
         <script async defer src="https://cusdis.com/js/cusdis.es.js"></script>  
+      </>
   )
 }
 export const Cusdis = React.memo(CusdisImpl)
