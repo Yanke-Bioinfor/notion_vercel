0d4d352a651c433fb1c1282ee9e5631a47717267
Yanke <37400776+Yanke-Bioinfor@users.noreply.github.com>
2024-01-23 16:43:30 +0800

    Update Cusdis.tsx

--- components/Cusdis.tsx
+++ components/Cusdis.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
+import * as types from '@/lib/types'
 
-export const CusdisImpl: React.FC = ({ pageId, canonicalPageUrl, title }) => {
+export const CusdisImpl: React.FC<types.PageProps> = ({ pageId, title }) => {
   return (
     <>
           <div id="cusdis_thread"
         data-host="https://cusdis.com"
         data-app-id="a260ec53-7bfc-46c1-a7ac-6e74bbd526d8"
         data-page-id={pageId}
-        data-page-url={canonicalPageUrl}
+        data-page-url={pageId}
         data-page-title={title}
         ></div>
         <script async defer src="https://cusdis.com/js/cusdis.es.js"></script>  
