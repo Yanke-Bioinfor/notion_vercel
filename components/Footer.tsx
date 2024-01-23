59ed4975a9aee0152309a0620747ef143d5ec31c
Yanke <37400776+Yanke-Bioinfor@users.noreply.github.com>
2024-01-23 14:03:18 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,17 +36,15 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div id="tcomment"></div>
-<script src="https://cdn.staticfile.org/twikoo/1.6.30/twikoo.all.min.js"></script>
-<script>
-twikoo.init({
-  envId: 'https://twikoo-yanke-f1lpm1cyk-yankes-projects.vercel.app/', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
-  el: '#tcomment', // 容器元素
-  // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
-  // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
-  // lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/twikoojs/twikoo/blob/main/src/client/utils/i18n/index.js
-})
-</script>
+<div id="cusdis_thread"
+  data-host="https://cusdis.com"
+  data-app-id="a260ec53-7bfc-46c1-a7ac-6e74bbd526d8"
+  data-page-id="{{ PAGE_ID }}"
+  data-page-url="{{ PAGE_URL }}"
+  data-page-title="{{ PAGE_TITLE }}"
+></div>
+<script async defer src="https://cusdis.com/js/cusdis.es.js"></script>
+
       <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
 
       <div className={styles.settings}>
